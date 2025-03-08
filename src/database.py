87a1952e8a1bcf6f4daef93c5af6948fa987d4d8
katsumi sahashi <1004596+ksahashi@users.noreply.github.com>
2025-03-08 23:58:30 +0900

    update structure (#4)
    * update
    
    * update
    
    * upate2
    
    * update
    
    * oops
    
    * update
    
    * update2
    
    * update3
    
    * update
    
    * update

--- src/database.py
+++ src/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from env import DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER
+from env import DB_HOST, DB_NAME, DB_PASSWORD, DB_USER
 
-DATABASE = f"mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8"
+DATABASE = f"mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}?charset=utf8"
 
 engine = create_engine(
     DATABASE,
