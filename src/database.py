309bf29cad3abe5668bd4999eebff8088d92bb99
katsumi sahashi <sahashi@dmotion.co.jp>
2025-03-09 11:09:40 +0900

    pytest warning を撲滅

--- src/database.py
+++ src/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import declarative_base, scoped_session, sessionmaker
 
 from env import DB_HOST, DB_NAME, DB_PASSWORD, DB_USER
 
